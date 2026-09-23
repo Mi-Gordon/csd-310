6e2034c0fd179cc37ca274a469ddd31541bb4d33
MikeGordon <migordon@my365.bellevue.edu>
2026-09-22 20:43:39 -0700

    Adding initial python code and word doc with required printouts

---
+++ module-9/outland_adventures.py
@@ -0,0 +1,67 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+import dotenv
+from dotenv import dotenv_values
+
+# Using the .env file
+secrets = dotenv_values(".env")
+
+# Database config object
+config = {
+    "user": secrets["USER"],
+    "password": secrets["PASSWORD"],
+    "host": secrets["HOST"],
+    "database": secrets["DATABASE"],
+    "raise_on_warnings": True
+}
+
+
+def show_table(cursor, table_name):
+    cursor.execute (f'SELECT * FROM {table_name}')
+
+    results = cursor.fetchall()
+
+    print('\n' + '*' *50)
+    print(table_name)
+    print('*' *50)
+
+    # Show each row
+    for row in results:
+        print(row)
+
+
+
+
+try:
+    db = mysql.connector.connect(**config)  # Connect to the movies database
+    # Create cursor
+    cursor = db.cursor()
+
+
+    # Show data from each table
+    show_table(cursor, 'Roles')
+    show_table(cursor, 'Customers')
+    show_table(cursor, 'Address')
+    show_table(cursor, 'Locations')
+    show_table(cursor, 'Categories')
+    show_table(cursor, 'Employees')
+    show_table(cursor, 'Trips')
+    show_table(cursor, 'Trips')
+    show_table(cursor, 'Bookings')
+    show_table(cursor, 'Equipment')
+    show_table(cursor, 'Equipment_Rentals')
+    show_table(cursor, 'Equipment_Sales')
+
+
+except mysql.connector.Error as err:
+    # On error code
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+    else:
+        print(err)
+finally:
+    # Close the connection
+    db.close()
